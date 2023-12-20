a5ba328d28fe51c5c7e70434aaab8687c3b8b101
Farid <abdoulfaridbassirou7898@gmail.com>
2023-12-20 13:07:22 +0100

    make some changes from MNC

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          initializing the project
+          So you are working now
         </a>
       </header>
     </div>
