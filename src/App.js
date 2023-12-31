e80c2210a9b8399b6a84cbcb03ae0a61f5157447
Farid <abdoulfaridbassirou7898@gmail.com>
2023-12-31 01:30:42 +0100

    create the nav component and background color added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          So you are working now
-        </a>
-      </header>
+    <Navigation style={{display:'flex',justifyContent:'flex-end'}}/>
+    {/*<Logo/>
+    <ImageLinkForm/>
+   <FaceRecognition/>*/} 
     </div>
   );
 }
