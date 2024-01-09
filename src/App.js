f1f6134bfba22dfaa57121b2d60052e615683217
Farid <abdoulfaridbassirou7898@gmail.com>
2024-01-09 17:38:10 +0100

    integrate the API

--- src/App.js
+++ src/App.js
@@ -4,13 +4,48 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import {Component} from 'react'
-// import Clarifai from 'clarifai';
 
-// const app = new Clarifai.App({
-//   apiKey: '8dfcb2c0b2ed47af8542e12cd016ea1c'
-// });
+const returnsetupClarifaiRequestOptions = (imageURL) =>{
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = 'f2e9e5cd97cf46edabb328d372ecabcd';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'baafbass';       
+    const APP_ID = 'smartbrain';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';    
+    const IMAGE_URL = imageURL;
 
-class  App extends Component{
+        const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+
+         const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+return requestOptions;
+}
+
+class App extends Component{
 
 constructor(){
   super();
@@ -25,8 +60,17 @@ onInputChange = (event) =>
 }
 
 onButtonSubmit = () => {
-  console.log('click')
-  // app.models.predict('6dc7e46bc9124c5c8824be4822abe105','https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/250px-Lionel_Messi_WC2022.jpg').then(
+  //console.log('click')
+
+fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnsetupClarifaiRequestOptions("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/250px-Lionel_Messi_WC2022.jpg"))
+  .then(response => response.json())
+        .then(result => console.log(result))
+
+
+  // app.models.predict(
+  //   "face-detection",
+  //   "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/250px-Lionel_Messi_WC2022.jpg")
+  // .then(
   // function(response)
   //   {
   //   console.log(response);
