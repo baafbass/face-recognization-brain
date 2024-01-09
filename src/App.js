49a2fca6526abd562b82f0fe808006a687b40ea7
Farid <abdoulfaridbassirou7898@gmail.com>
2024-01-09 15:20:16 +0100

    trying to install Clarifai API

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import {Component} from 'react'
+// import Clarifai from 'clarifai';
 
+// const app = new Clarifai.App({
+//   apiKey: '8dfcb2c0b2ed47af8542e12cd016ea1c'
+// });
 
-class App extends Component {
+class  App extends Component{
 
 constructor(){
   super();
@@ -20,8 +24,18 @@ onInputChange = (event) =>
   console.log(event.target.value);
 }
 
-onButtonSubmit = ()=>{
-  console.log('Click');
+onButtonSubmit = () => {
+  console.log('click')
+  // app.models.predict('6dc7e46bc9124c5c8824be4822abe105','https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/250px-Lionel_Messi_WC2022.jpg').then(
+  // function(response)
+  //   {
+  //   console.log(response);
+  //   },
+  //   function(err)
+  //   {
+
+  //   }
+  //   );
 }
 
   render(){
