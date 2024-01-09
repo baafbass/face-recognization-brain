e5c7b7474eda405105f87874cfa5701f0e2a69ea
Farid <abdoulfaridbassirou7898@gmail.com>
2024-01-09 11:14:28 +0100

    onInputChange and onSubmitChange create

--- src/App.js
+++ src/App.js
@@ -3,40 +3,42 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-// import Particles from 'react-particles';
-// import Particles from "react-tsparticles";
-
-// const particlesOptions = {
-//                 particles: {
-//                  Number: {
-//                   value: 30,
-//                   density: {
-//                     enable:true,
-//                     value_area: 800
-//                   }
-//                  }
-//                 }
-//               }
+import {Component} from 'react'
 
 
-function App() {
-  return (
+class App extends Component {
+
+constructor(){
+  super();
+  this.state = {
+    input : '',
+  }
+}
+
+onInputChange = (event) =>
+{
+  console.log(event.target.value);
+}
+
+onButtonSubmit = ()=>{
+  console.log('Click');
+}
+
+  render(){
+      return (
     <div className="App">
-    {/*<Particles className='particles'
-              params={particlesOptions}
-            />*/}
-
-{/* hhh<Particles id="tsparticles" url="http://foo.bar/particles.json"/>*/}
-
     <Navigation />
     <Logo/>
     <Rank/>
-    <ImageLinkForm/>
+    <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit}/>
     {/*
     
    <FaceRecognition/>*/} 
     </div>
   );
 }
+
+  }
+
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css'
-const ImageLinkForm = ()=>{
+
+const ImageLinkForm = ({onInputChange,onButtonSubmit})=>{
 
 	return(
           <div>
@@ -9,8 +10,11 @@ const ImageLinkForm = ()=>{
           	</p>
           	<div className='center'>
           	<div className='form center pa4 br-3 shadow-5'>
-          		<input type='text' className='f4 pa2 w-70 center'/>
-          		<button className='f4 grow w-30 ph3 link pv2 dib white bg-light-purple'>Detect</button>
+          		<input type='text' className='f4 pa2 w-70 center' onChange={onInputChange}/>
+          		<button 
+          		className='f4 grow w-30 ph3 link pv2 dib white bg-light-purple'
+                onClick = {onButtonSubmit}
+          		>Detect</button>
           	</div>
           	</div>
           </div>
