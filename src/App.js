5aedcf5e6c7b3da859c28fffdb7cdb5b5c3fc737
Farid <abdoulfaridbassirou7898@gmail.com>
2024-01-11 12:58:13 +0100

    Register form get created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import {Component} from 'react'
 
@@ -100,15 +101,15 @@ onRouteChange = (route)=>{
     <div className="App">
     <Navigation onRouteChange={this.onRouteChange}/>
 
-    { this.state.route === 'SignIn' 
-    ? <SignIn onRouteChange = {this.onRouteChange}/> 
-    : 
-    <div>
+    { this.state.route === 'home' 
+    ?     <div>
     <Logo/>
     <Rank/>
     <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit}/>
     <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/> 
     </div>
+    : (this.state.route === 'SignIn' ? <SignIn onRouteChange = {this.onRouteChange}/> : <Register onRouteChange = {this.onRouteChange}/>) 
+
   }
   </div>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const SignIn = ({onRouteChange}) => {
-   return(
-   	<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+const Register = ({onRouteChange}) =>{
+	return(
+<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 	    <main className="pa4 black-80">
 	  <form className="measure">
 	    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-	      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+	      <legend className="f1 fw6 ph0 mh0">Register</legend>
+	      <div className="mt3">
+	        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+	        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
+	      </div>
 	      <div className="mt3">
 	        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 	        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
@@ -21,17 +25,13 @@ const SignIn = ({onRouteChange}) => {
 	      onClick={()=>onRouteChange('home')}
 	      className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 	      type="submit" 
-	      value="Sign in"/>
-	    </div>
-	    <div className="lh-copy mt3">
-	      <p 
-	       onClick={()=>onRouteChange('Register')}
-	      className="f6 link dim black db pointer">Register</p>
+	      value="Register"/>
 	    </div>
 	  </form>
 	</main>
     </article>
-   	);
-}
 
-export default SignIn;
+
+		);
+}
+export default Register;
